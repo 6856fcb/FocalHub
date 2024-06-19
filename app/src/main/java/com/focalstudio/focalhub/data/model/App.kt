495558d39dd0235a49aa398e04ff258e5628bfb7
lukatr1 <luka.trifunovic@stud.fh-campuswien.ac.at>
2024-06-19 20:29:21 +0200

    small fixes

--- app/src/main/java/com/focalstudio/focalhub/data/model/App.kt
+++ app/src/main/java/com/focalstudio/focalhub/data/model/App.kt
@@ -3,7 +3,6 @@ package com.focalstudio.focalhub.data.model
 import android.graphics.drawable.Drawable
 
 data class App(
-
     val name: String,
     val packageName: String,
     val icon: Drawable
